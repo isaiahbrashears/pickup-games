cb1e4dffdf7fd9e702476efd54a76bceac538edb
Isaiah Brashears <izzyb@Isaiahs-MacBook-Pro.local>
2019-12-16 16:03:03 -0600

    .

--- src/main/java/com/codeup/blog/blog/models/Comment.java
+++ src/main/java/com/codeup/blog/blog/models/Comment.java
@@ -1,11 +1,64 @@
 package com.codeup.blog.blog.models;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "comments")
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(250)")
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn (name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn (name = "game_id")
+    private Game game;
+
+    public Comment() {}
+
+    public Comment(String comment, User user, Game game) {
+        this.comment = comment;
+        this.user = user;
+        this.game = game;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
 }
